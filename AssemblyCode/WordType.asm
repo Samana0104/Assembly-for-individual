eb0e08d37b94b0463f0d7902a21b12b54ef90782
HanBit Byeon <b_hb0104@naver.com>
2023-04-01 22:10:03 +0900

    modify the code and annotation

--- AssemblyCode/WordType.asm
+++ AssemblyCode/WordType.asm
@@ -11,7 +11,13 @@ CMAIN:
     ret
     
 section .bss ; ram .bss segmentation
-    a db 0x33 ; db 1byte
-    b dw 0x1234 ; dw 2byte
-    c dd 0x12345678 ; dd 4byte
-    d dq 0x1234 ; dq 8byte
+    ba resb 1 ; 1byte 크기 a 변수 1개 선언
+    bb resw 2; 2byte 크기 b 변수 2개 선언
+    bc resd 1; 4byte
+    bd resq 3 ; 8byte
+    
+section .data
+    a db 0x33 ; 1byte a 변수 선언 및 0x33으로 초기화
+    b dw 0x1234 ; 2byte b 변수 선언 및 0x1234로 초기화
+    c dd 0x12345678 ; 4byte
+    d dq 0x1234 ; 8byte 
